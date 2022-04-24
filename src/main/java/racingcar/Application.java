3da847c692bba0e432426bc73cc1e8f744e20f1c
berrynana <parkjiyoon7@gmail.com>
2022-04-24 20:38:54 +0900

    feat(feature): call playRacingCar of RacingCarService class

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import service.RacingCarService;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingCarService service = new RacingCarService();
+        service.playRacingCar();
     }
 }
